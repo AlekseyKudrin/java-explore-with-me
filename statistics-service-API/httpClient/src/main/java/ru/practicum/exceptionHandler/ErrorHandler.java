3acc688e8e1a8a4074de08393727b8953e5a187b
frade <AlekseyKudrin91@yandex.ru>
2023-05-20 23:22:35 +0300

    fix 1.3

--- statistics-service-API/httpClient/src/main/java/ru/practicum/exceptionHandler/ErrorHandler.java
+++ statistics-service-API/httpClient/src/main/java/ru/practicum/exceptionHandler/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.practicum.exceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,7 +13,7 @@ public class ErrorHandler {
 
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handlerIllegalArgumentException(final IllegalArgumentException e) {
+    public ErrorResponse handlerMethodArgumentNotValidException(final MethodArgumentNotValidException e) {
         log.error("Data input incorrect: {}", e.getMessage());
         return new ErrorResponse(e.getMessage());
     }
