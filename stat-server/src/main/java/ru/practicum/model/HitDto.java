8da5ecdc1b3a8767cd3c2f92bb31a26093bfd215
Kudrin <alekseykudrin91@yandex.ru>
2023-06-15 15:27:29 +0300

    clean

---
+++ stat-server/src/main/java/ru/practicum/model/HitDto.java
@@ -0,0 +1,22 @@
+package ru.practicum.model;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@Setter
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class HitDto {
+    @NotBlank
+    String app;
+    @NotBlank
+    String uri;
+    @NotBlank
+    String ip;
+    @NotBlank
+    String timestamp;
+}
